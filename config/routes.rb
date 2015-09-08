8d97d5f67a6f8b912a4a3a6ec557162972daa40d
Brian Strasburg <bstras21@gmail.com>
2015-09-08 13:03:31 -0600

    Added HTML Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'html_pages/home'
+
+  get 'html_pages/about'
+
+  get 'html_pages/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
